c7bfade431368df5ae32a5879fcf628462e81d32
THIAGOL7 <104383248+THIAGOL7@users.noreply.github.com>
2023-06-12 16:11:28 -0300

    Create mainCamila.dart

---
+++ mainCamila.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+import 'package:aula_913/Refeicoes.dart';
+
+void main() {
+
+ runApp(
+   MaterialApp(
+
+     home: Refeicoes(),
+     debugShowCheckedModeBanner: false ,
+
+   ),
+ );
+}
